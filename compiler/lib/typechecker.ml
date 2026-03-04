2dd1d73b13956b259c3882f0b3b0966efe309e76
Ramon Asuncion <asuncionbatista@gmail.com>
2026-03-04 16:33:25 -0500

    Start checking function

--- compiler/lib/typechecker.ml
+++ compiler/lib/typechecker.ml
@@ -5,6 +5,8 @@ open Types
 
 exception TypeError of string
 
+(* TODO: Figure out "-typecheck" with better Printf formatting *)
+
 type func_sig = { param_tys : ty list; ret_ty : ty }
 type struct_info = { field_tys : (string * ty) list }
 
@@ -44,21 +46,22 @@ let rec ty_of_ast (env : env) (t : typ) : ty =
 (* First pass collecting signatures so that the compiler
    can handle forward references *)
 let collect_func (env : env) (fd : func_def) : unit =
-  Printf.printf "Signature: %s\n%!" fd.name;
+  (* Printf.printf "Signature: %s\n%!" fd.name; *)
 
-  (* Map AST to internal type to avoid reparsing *)
+  (* Map AST to internal type to define the functions global
+  signature *)
   let param_tys =
     List.map
       (fun (p : param) ->
         let t = ty_of_ast env p.typ in
-        Printf.printf "%s: %s\n%!" p.name (show_ty t);
+        (* Printf.printf "%s: %s\n%!" p.name (show_ty t); *)
         t)
       fd.params
   in
 
   (* Default to void type when user omits the return type *)
   let ret_ty = match fd.ret with Some t -> ty_of_ast env t | None -> TVoid in
-  Printf.printf "returns: %s\n%!" (show_ty ret_ty);
+  (* Printf.printf "returns: %s\n%!" (show_ty ret_ty); *)
 
   Hashtbl.replace env.funcs fd.name { param_tys; ret_ty }
 
@@ -74,7 +77,27 @@ let collect_decl (env : env) (decl : decl) : unit =
   | Struct sd -> collect_struct env sd
   | Func fd | Extern fd -> collect_func env fd
 
+(* Second pass doing the bidirectional type checking *)
+
+let check_func (env : env) (fd : func_def) : unit =
+  (* Map AST to internal type (again) only for this function check *)
+  let params =
+    List.map (fun (p : param) -> (p.name, ty_of_ast env p.typ)) fd.params
+  in
+
+  (* let param_env =
+  in *)
+  ()
+
+let check_decl (env : env) (decl : decl) : unit =
+  match decl with
+  | Func fd ->
+      let _tfd = check_func env fd in
+      ()
+  | _ -> ()
+
 let typecheck (_decls : decl list) : unit =
   let _env = make_env () in
   List.iter (collect_decl _env) _decls;
+  List.map (check_decl _env) _decls;
   ()
