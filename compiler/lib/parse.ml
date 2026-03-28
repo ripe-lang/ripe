bbf3574a8865d6317f9ad8b546a618464e3e0be7
Ramon Asuncion <asuncionbatista@gmail.com>
2026-03-28 11:46:19 -0400

    Scaffold recursive descent parser

--- compiler/lib/parse.ml
+++ compiler/lib/parse.ml
@@ -5,7 +5,33 @@ open Ast
 
 exception ParseError of string
 
+type state = {
+  mutable tok : token;
+  lexbuf : Lexing.lexbuf;
+  read : Lexing.lexbuf -> token;
+}
+
+let advance st = st.tok <- st.read st.lexbuf
+
+let skip_semi st =
+  while st.tok = SEMI do
+    advance st
+  done
+
+let parse_decl st =
+  skip_semi st;
+  match st.tok with _ -> raise (ParseError "Expected declaration")
+
+let parse_program st =
+  let decls = ref [] in
+  skip_semi st;
+  while st.tok <> EOF do
+    decls := parse_decl st :: !decls;
+    skip_semi st
+  done;
+  List.rev !decls
+
 let parse (read : Lexing.lexbuf -> Tokens.token) (lexbuf : Lexing.lexbuf) :
     Ast.decl list =
-  ignore (read, lexbuf);
-  failwith "TODO: recursive descent parser"
+  let st = { tok = read lexbuf; lexbuf; read } in
+  parse_program st
