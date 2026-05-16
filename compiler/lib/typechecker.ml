948ae9f4674503f83ee38d7a630d5fb26ba5c9d0
Ramon Asuncion <asuncionbatista@gmail.com>
2026-05-16 19:47:55 -0400

    Collect all type errors before reporting

--- compiler/lib/typechecker.ml
+++ compiler/lib/typechecker.ml
@@ -5,7 +5,7 @@
 open Ast
 open Types
 
-exception TypeError of string
+exception TypeErrors of string list
 
 (* TODO(5c2a): Figure out "-typecheck" with better Printf formatting *)
 (* TODO(38df): I need to update Parser to allow `let` and `var` in toplevel
@@ -29,16 +29,32 @@ type env = {
   structs : (string, struct_info) Hashtbl.t;
   ret_ty : ty;
   in_loop : bool;
+  errors : string list ref;
+  filename : string;
+  sm : Source_map.t;
+  current_span : Ast.span ref;
 }
 
-let make_env () : env =
+let make_env (filename : string) (sm : Source_map.t) : env =
   {
     vars = [];
     funcs = Hashtbl.create 16;
     structs = Hashtbl.create 16;
     ret_ty = TVoid;
     in_loop = false;
+    errors = ref [];
+    filename;
+    sm;
+    current_span = ref Ast.dummy_span;
   }
+
+let add_error (env : env) (msg : string) : unit =
+  let span = !(env.current_span) in
+  let line, col = Source_map.lookup env.sm span.lo in
+  let formatted = Printf.sprintf "%s:%d:%d: %s" env.filename line col msg in
+  env.errors := formatted :: !(env.errors)
+
+let dummy_texpr = Typed_ast.TInt (0, TInt I32)
 
 let extend_var (env : env) (name : string) (t : ty) : env =
   { env with vars = (name, t) :: env.vars }
@@ -46,17 +62,23 @@ let extend_var (env : env) (name : string) (t : ty) : env =
 let lookup_var (env : env) (name : string) : ty =
   match List.assoc_opt name env.vars with
   | Some t -> t
-  | None -> raise (TypeError ("unbound variable: " ^ name))
+  | None ->
+      add_error env ("undefined variable '" ^ name ^ "'");
+      TInt I32
 
 let lookup_func (env : env) (name : string) : func_sig =
   match Hashtbl.find_opt env.funcs name with
   | Some s -> s
-  | None -> raise (TypeError ("unknown function: " ^ name))
+  | None ->
+      add_error env ("undefined function '" ^ name ^ "'");
+      { param_tys = []; ret_ty = TVoid }
 
 let lookup_struct (env : env) (name : string) : struct_info =
   match Hashtbl.find_opt env.structs name with
   | Some s -> s
-  | None -> raise (TypeError ("unknown struct: " ^ name))
+  | None ->
+      add_error env ("undefined struct '" ^ name ^ "'");
+      { field_tys = [] }
 
 let rec ty_of_ast (env : env) (t : typ) : ty =
   match t.tdesc with
@@ -73,7 +95,10 @@ let rec ty_of_ast (env : env) (t : typ) : ty =
   | Named "bool" -> TBool
   | Named name ->
       if Hashtbl.mem env.structs name then TStruct name
-      else raise (TypeError ("unknown type: " ^ name))
+      else (
+        env.current_span := t.span;
+        add_error env ("undefined type '" ^ name ^ "'");
+        TInt I32)
   | Pointer t -> TPointer (ty_of_ast env t)
 
 (* Exact equality but NULL is compatible with any pointer *)
@@ -121,13 +146,15 @@ let collect_func (env : env) (fd : func_def) : unit =
 (* This will fail if Struct A has a field of type Struct B and B is defined after A *)
 (* FIXME: Add DFS cycle detection to prevent infinite recursion*)
 let collect_struct (env : env) (sd : struct_def) : unit =
-  if Hashtbl.mem env.structs sd.name then
-    raise (TypeError ("struct already defined: " ^ sd.name));
-  (* TODO(9b1e): Add a rawptr/voidptr keyword for untyped pointers (C's void pointer) *)
-  let field_tys =
-    List.map (fun (f : field) -> (f.name, ty_of_ast env f.typ)) sd.fields
-  in
-  Hashtbl.replace env.structs sd.name { field_tys }
+  if Hashtbl.mem env.structs sd.name then (
+    env.current_span := sd.span;
+    add_error env ("'" ^ sd.name ^ "' is already defined"))
+  else
+    (* TODO(9b1e): Add a rawptr/voidptr keyword for untyped pointers (C's void pointer) *)
+    let field_tys =
+      List.map (fun (f : field) -> (f.name, ty_of_ast env f.typ)) sd.fields
+    in
+    Hashtbl.replace env.structs sd.name { field_tys }
 
 let collect_decl (env : env) (decl : decl) : unit =
   match decl with
@@ -138,11 +165,11 @@ let collect_decl (env : env) (decl : decl) : unit =
 
 (* Figure out the type*)
 let rec synth (env : env) (e : expr) : Typed_ast.texpr =
+  env.current_span := e.span;
   match e.desc with
   | Int n ->
       (* Printf.printf "int %d\n" n; *)
       Typed_ast.TInt (n, TInt I32)
-      (* TODO(fa2c): Widen based on value magnitude (fall back I32) *)
   | Float f -> Typed_ast.TFloat (f, TFloat F64)
   | Bool b ->
       (* Printf.printf "bool %b\n" b; *)
@@ -166,7 +193,7 @@ let rec synth (env : env) (e : expr) : Typed_ast.texpr =
       Typed_ast.TCall (name, targs, sig_.ret_ty)
   | BinOp (op, l, r) -> synth_binop env op l r
   | UnOp (op, e) -> synth_unop env op e
-  | FieldAccess (e, fname) -> synth_field env e fname
+  | FieldAccess (inner_e, fname) -> synth_field env inner_e fname
   | Cast (e, t) ->
       let te = synth env e in
       let ty = ty_of_ast env t in
@@ -196,6 +223,7 @@ let rec synth (env : env) (e : expr) : Typed_ast.texpr =
 
 (* MUST be this type *)
 and check (env : env) (e : expr) (want : ty) : Typed_ast.texpr =
+  env.current_span := e.span;
   match e.desc with
   | Int n -> (
       (* TODO(0ab1): Validate n fits within want (e.g. reject 300 into u8). Also, inferred literals still default to I32 large values overflow. *)
@@ -203,27 +231,24 @@ and check (env : env) (e : expr) (want : ty) : Typed_ast.texpr =
       | TInt _ -> Typed_ast.TInt (n, want)
       (* want is not an integer type at all e.g. let y: bool = 20 *)
       | _ ->
-          raise
-            (TypeError
-               (Printf.sprintf "type mismatch: expected %s, got (TInt I32)"
-                  (show_ty want))))
+          add_error env
+            (Printf.sprintf "expected %s but found i32" (show_ty want));
+          Typed_ast.TInt (n, TInt I32))
   | Float f -> (
       match want with
       | TFloat _ -> Typed_ast.TFloat (f, want)
       | _ ->
-          raise
-            (TypeError
-               (Printf.sprintf "type mismatch: expected %s, got (TFloat F64)"
-                  (show_ty want))))
+          add_error env
+            (Printf.sprintf "expected %s but found f64" (show_ty want));
+          Typed_ast.TFloat (f, TFloat F64))
   (* not a flexible literal, just check it matches want *)
   | _ ->
       let te = synth env e in
       let got = Typed_ast.ty_of_texpr te in
       if not (compatible want got) then
-        raise
-          (TypeError
-             (Printf.sprintf "type mismatch: expected %s, got %s" (show_ty want)
-                (show_ty got)));
+        add_error env
+          (Printf.sprintf "expected %s but found %s" (show_ty want)
+             (show_ty got));
       te
 
 and check_args (env : env) (sig_ : func_sig) (args : expr list) :
@@ -231,12 +256,11 @@ and check_args (env : env) (sig_ : func_sig) (args : expr list) :
   (* TODO(5038): Support for variadic functions *)
   let n_params = List.length sig_.param_tys in
   let n_args = List.length args in
-  if n_params <> n_args then
-    raise
-      (TypeError
-         (Printf.sprintf "wrong number of arguments: expected %d, got %d"
-            n_params n_args));
-  List.map2 (fun e want -> check env e want) args sig_.param_tys
+  if n_params <> n_args then (
+    add_error env
+      (Printf.sprintf "expected %d arguments but got %d" n_params n_args);
+    [])
+  else List.map2 (fun e want -> check env e want) args sig_.param_tys
 
 (* let is_numeric = function
     | TInt _ | TFloat -> true
@@ -289,7 +313,6 @@ and synth_binop (env : env) (op : binop) (l : expr) (r : expr) : Typed_ast.texpr
       let tl = synth env l in
       (* if not (is_lvalue tl) then
         raise (TypeError "left-hand side of assignment must be an lvalue"); *)
-
       let t = Typed_ast.ty_of_texpr tl in
       let tr = check env r t in
       Typed_ast.TBinOp (op, tl, tr, t)
@@ -314,8 +337,9 @@ and synth_unop (env : env) (op : unop) (e : expr) : Typed_ast.texpr =
       match Typed_ast.ty_of_texpr te with
       | TPointer inner -> Typed_ast.TUnOp (op, te, inner)
       | t ->
-          raise
-            (TypeError ("cannot dereference non-pointer type: " ^ show_ty t)))
+          add_error env
+            ("cannot dereference non-pointer type '" ^ show_ty t ^ "'");
+          dummy_texpr)
   | AddressOf ->
       let te = synth env e in
       Typed_ast.TUnOp (op, te, TPointer (Typed_ast.ty_of_texpr te))
@@ -324,19 +348,27 @@ and synth_unop (env : env) (op : unop) (e : expr) : Typed_ast.texpr =
 and synth_field (env : env) (e : expr) (fname : string) : Typed_ast.texpr =
   let te = synth env e in
   let rec peel = function
-    | TStruct sname -> sname
+    | TStruct sname -> Some sname
     | TPointer t -> peel t
-    | t -> raise (TypeError ("field access on non-struct type: " ^ show_ty t))
+    | _ -> None
   in
-  let sname = peel (Typed_ast.ty_of_texpr te) in
-  let info = lookup_struct env sname in
-  match List.assoc_opt fname info.field_tys with
-  | Some ft -> Typed_ast.TFieldAccess (te, fname, ft)
-  | None -> raise (TypeError ("struct " ^ sname ^ " has no field " ^ fname))
+  let ty = Typed_ast.ty_of_texpr te in
+  match peel ty with
+  | None ->
+      add_error env ("type '" ^ show_ty ty ^ "' has no fields");
+      dummy_texpr
+  | Some sname -> (
+      let info = lookup_struct env sname in
+      match List.assoc_opt fname info.field_tys with
+      | Some ft -> Typed_ast.TFieldAccess (te, fname, ft)
+      | None ->
+          add_error env ("'" ^ sname ^ "' has no field '" ^ fname ^ "'");
+          dummy_texpr)
 
 (* TODO(ccf6): Validate that the operand is a numeric type *)
 (* TODO(b5ae): Better error messages *)
 let rec check_stmt (env : env) (s : stmt) : env * Typed_ast.tstmt =
+  env.current_span := s.span;
   match s.sdesc with
   | Let (name, ann, e) ->
       let t, te =
@@ -362,12 +394,13 @@ let rec check_stmt (env : env) (s : stmt) : env * Typed_ast.tstmt =
             (Typed_ast.ty_of_texpr te, te)
         | Some _, None -> failwith "zero-init var not yet implemented"
         | None, None ->
-            raise (TypeError (Printf.sprintf "cannot infer type for '%s'" name))
+            add_error env (Printf.sprintf "cannot infer type of '%s'" name);
+            (TInt I32, dummy_texpr)
       in
       (extend_var env name t, Typed_ast.TVar (name, t, te))
   | Return None ->
       if env.ret_ty <> TVoid then
-        raise (TypeError "empty return in non-void function");
+        add_error env "empty return in non-void function";
       (env, Typed_ast.TReturn None)
   | Return (Some e) ->
       let te = check env e env.ret_ty in
@@ -399,12 +432,10 @@ let rec check_stmt (env : env) (s : stmt) : env * Typed_ast.tstmt =
       in
       (env, Typed_ast.TFor (name, elem_ty, titer, tbody))
   | Break ->
-      if not env.in_loop then
-        raise (TypeError "break statement must be inside a loop");
+      if not env.in_loop then add_error env "break outside loop";
       (env, Typed_ast.TBreak)
   | Continue ->
-      if not env.in_loop then
-        raise (TypeError "continue statement must be inside a loop");
+      if not env.in_loop then add_error env "continue outside loop";
       (env, Typed_ast.TContinue)
   | Block stmts ->
       let _, tstmts = check_stmts env stmts in
@@ -464,12 +495,18 @@ let check_decl (env : env) (decl : decl) : Typed_ast.tdecl =
       let tfd = check_func env fd in
       Typed_ast.TExtern tfd
   | Struct sd ->
+      env.current_span := sd.span;
       let info = lookup_struct env sd.name in
       Typed_ast.TStruct (sd.name, info.field_tys, sd.modifiers)
 (* | _ -> failwith "Declaration not supported yet" *)
 (* TODO(880b): I need to think about global variables *)
 
-let typecheck (decls : decl list) : Typed_ast.tdecl list =
-  let env = make_env () in
+let typecheck (filename : string) (src : string) (decls : decl list) :
+    Typed_ast.tdecl list =
+  let sm = Source_map.create src in
+  let env = make_env filename sm in
   List.iter (collect_decl env) decls;
-  List.map (check_decl env) decls
+  let tdecls = List.map (check_decl env) decls in
+  match List.rev !(env.errors) with
+  | [] -> tdecls
+  | errors -> raise (TypeErrors errors)
