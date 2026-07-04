0ed459ec747b69237bb828fbab404b07dc66a48f
Ramon Asuncion <asuncionbatista@gmail.com>
2026-07-04 02:20:03 -0400

    Change formatting for spans

--- lib/ast.ml
+++ lib/ast.ml
@@ -4,9 +4,8 @@
 (* I was going to wait before doing byte offset but the dump-ast was way too long. *)
 type span = { lo : int; hi : int }
 
-(* spans elided from dumps so derived show stays readable *)
-let pp_span fmt (_ : span) = Format.pp_print_string fmt "_"
-let show_span (_ : span) = "_"
+let pp_span fmt { lo; hi } = Format.fprintf fmt "(%d,%d)" lo hi
+let show_span s = Format.asprintf "%a" pp_span s
 let dummy_span = { lo = 0; hi = 0 }
 
 type typ_desc =
