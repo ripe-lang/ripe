f7603719a701d6d78561ae4d0c8d0188dbcf99a0
Ramon Asuncion <asuncionbatista@gmail.com>
2026-07-07 04:32:59 -0400

    Add symbol module

---
+++ lib/symbol.ml
@@ -0,0 +1,14 @@
+(* SPDX-License-Identifier: GPL-2.0-only *)
+
+(* TODO: A counter is fine since I only compile one file but when I add
+   modules I will need mangled names for the linker+ABI on top of the ids*)
+type id = int [@@deriving show { with_path = false }]
+
+type kind = Func | Extern | Global | Const | Var | Param | ForVar
+[@@deriving show { with_path = false }]
+
+type t = { id : id; name : string; kind : kind; span : Ast.span }
+[@@deriving show { with_path = false }]
+
+let is_func = function Func | Extern -> true | _ -> false
+let is_global = function Global -> true | _ -> false
