861fe9cbadbc097859d340d2fdcf4c52e5fc9f18
Ramon Asuncion <asuncionbatista@gmail.com>
2026-07-12 02:16:01 -0400

    Add a bounds panic to the runtime

--- runtime/panic.c
+++ runtime/panic.c
@@ -5,3 +5,8 @@ void ripe_panic(const char *msg) {
     fprintf(stderr, "panic: %s\n", msg);
     abort();
 }
+
+void ripe_panic_bounds(long idx, long len) {
+    fprintf(stderr, "panic: index %ld is out of range for length %ld\n", idx, len);
+    abort();
+}
