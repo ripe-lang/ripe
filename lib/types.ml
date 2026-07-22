0ab6bc50225248d60cedf6a8828c992898139157
Ramon Asuncion <asuncionbatista@gmail.com>
2026-07-21 22:29:07 -0400

    Add the any pointer type

--- lib/types.ml
+++ lib/types.ml
@@ -14,6 +14,7 @@ type ty =
   | TNever
   | TNull
   | TPointer of ty
+  | TAnyPtr
   | TStruct of string * ty list
   | TFunc of ty list * ty
   | TArray of ty * int
@@ -67,6 +68,7 @@ let rec show_ty = function
   | TNever -> "never"
   | TNull -> "null"
   | TPointer t -> "*" ^ show_ty t
+  | TAnyPtr -> "*any"
   | TStruct (name, []) -> name
   | TStruct (name, args) ->
       Printf.sprintf "%s[%s]" name (String.concat ", " (List.map show_ty args))
@@ -126,7 +128,7 @@ let rec ty_align (structs : (string, (string * ty) list) Hashtbl.t) (t : ty) :
   | TInt k -> int_kind_size k
   | TFloat k -> float_kind_size k
   | TBool -> 1
-  | TPointer _ | TNull | TCStr | TFunc _ -> 8
+  | TPointer _ | TAnyPtr | TNull | TCStr | TFunc _ -> 8
   | TVoid -> Error.ice "TVoid has no alignment"
   | TNever -> Error.ice "TNever has no alignment"
   | TError -> Error.ice "TError has no alignment"
@@ -151,7 +153,7 @@ let rec ty_size (structs : (string, (string * ty) list) Hashtbl.t) (t : ty) :
   | TInt k -> int_kind_size k
   | TFloat k -> float_kind_size k
   | TBool -> 1
-  | TPointer _ | TNull | TCStr | TFunc _ -> 8
+  | TPointer _ | TAnyPtr | TNull | TCStr | TFunc _ -> 8
   | TVoid -> Error.ice "TVoid has no size"
   | TNever -> Error.ice "TNever has no size"
   | TError -> Error.ice "TError has no size"
@@ -188,7 +190,8 @@ let is_scalar t =
 (* the 8 byte value classes, so constant folding picks a 64 bit result *)
 let is_wide_ty t =
   match resolve_ty t with
-  | TInt (I64 | U64 | Isize | Usize) | TPointer _ | TNull | TCStr | TFunc _ ->
+  | TInt (I64 | U64 | Isize | Usize)
+  | TPointer _ | TAnyPtr | TNull | TCStr | TFunc _ ->
       true
   | _ -> false
 
