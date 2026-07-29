316f0df861635f4bd1ad7856f0984b8b51806788
Ramon Asuncion <asuncionbatista@gmail.com>
2026-07-29 00:12:37 -0400

    Add pass interfaces

---
+++ lib/typechecker.mli
@@ -0,0 +1,5 @@
+(* SPDX-License-Identifier: GPL-2.0-only *)
+
+(* The partial tree stays available so later checks can still run *)
+val typecheck :
+  diags:Diagnostic.sink -> Resolve.t -> Ast.decl list -> Typed_ast.tdecl list
